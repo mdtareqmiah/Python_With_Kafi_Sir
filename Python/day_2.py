6cdc1c6ea6a13fceac6221002e62949d9c200158
Md Tareq Miah <mdtareqmia25@gmail.com>
2025-07-09 12:21:35 +0600

    python with kafi sir Day of second

---
+++ Python/day_2.py
@@ -0,0 +1,63 @@
+print("This is day of two")
+
+"""Python data type"""
+"""
+text type: str
+numeric type: int, float, complex
+sequence type: list, tuple, range
+mapping type: dict
+set type: set, frozenset
+boolean type: bool
+binary type: bytes, bytearray,nemoryview
+none type: none type 
+"""
+x=6
+print(type(x))
+x=1j
+print(type(x))
+x=["Tareq, Garia Jahangir, Moksud"]
+print(x)
+print(type(x))
+x=range(6)
+print(type(x))
+x={"name" : "Tareq", "age" :36, "class" : 12}
+print(x,type(x))
+x=int(20.0)
+print(x, type(x))
+x=memoryview(bytes(5))
+print(x, type(x))
+"""
+pytho  numbers
+int, float, complex
+"""
+x=12
+y=759345375937537537534573495375573
+z=-43753759375937
+print(type(x))
+print(type(y))
+print(type(z))
+x=35e3
+y=12E43
+z=-87.7e543
+print(type(x))
+print(type(y))
+print(type(z))
+x=1.20
+y=111.-131242343242
+z=-35.4543
+print(type(x))
+print(type(y))
+print(type(z))
+x=1232423
+y=234.248
+z=47j
+a=float(x)
+b=int(y)
+c=complex(x)
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+import random
+print(random.randrange(3,6))
+x=int(1)
+
