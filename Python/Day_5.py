1484e54389760e9e2051f576e0779319335a762f
Md Tareq Miah <mdtareqmia25@gmail.com>
2025-07-15 12:12:20 +0600

    string

--- Python/Day_5.py
+++ Python/Day_5.py
@@ -3,3 +3,18 @@
 print("It's alright")
 print("He is called 'Johnny'")
 print('He is called "Johnny"')
+
+"""Multi line String"""
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+#string are arrays
+a = "Hello, World!"
+print(a[1])
+
+#string looping through
+for x in "banana":
+  print(x)
