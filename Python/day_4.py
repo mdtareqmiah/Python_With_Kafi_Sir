2dbb4d14e6c2a4b96afdd5171010d56ebc4de37e
Md Tareq Miah <mdtareqmia25@gmail.com>
2025-07-15 11:30:40 +0600

    This is day of last day in week

---
+++ Python/day_4.py
@@ -0,0 +1,52 @@
+print("Jekunu kichu print kora jabe")
+s =20
+print(s, type(s))
+s="20"
+print(s, type(s))
+s=20.0
+print(s, type(s))
+x="30"
+b=40
+s=(s+b)
+print(s, type(s))
+#this is comment 
+#here is start type custing
+"""
+
+er modder ja 
+
+likbo 
+sob 
+comment
+hoyr 
+jabe
+
+"""
+print(int(s))
+#print(s+int(x))
+y=s+int(x)
+print(int(y))
+str(s)
+#print(s+x)
+n= "jahangir"
+m="alam"
+print(n,m)
+a=20
+f="30"
+a=str(a)
+print(a+f)
+d=23j
+print(d, type(d))
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+a=5
+A=7
+print(a+A)
+
+
+
