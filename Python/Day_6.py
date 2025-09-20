d79c5abe60cbee866636f5b825cd1156ce496525
Md Tareq Miah <mdtareqmia25@gmail.com>
2025-09-20 11:23:01 +0600

    List day_6

---
+++ Python/Day_6.py
@@ -0,0 +1,28 @@
+"""Hello This is day 6 for python lists"""
+
+mylist=["apple","banana", "cherry"]
+print(mylist)
+print(len(mylist))
+
+this_list=["this ", "is", "a", "list"]
+print(len(this_list))
+print(this_list)
+
+num_list=["1","2","3","4","5"]
+print(len(num_list))
+print(num_list)
+
+list1=["Asif", "Mahmud", "garia Jahangir", "sir"]
+list2=[1,2,3,4,5,6,7]
+list3=[True, False,False]
+
+print(list1)
+print(list2)
+print(list3)
+
+list1=["asif",34,True,40,"Female"]
+print(list1)
+print(len(list1))
+
+mylist=list(("apple","banana","cherry","list using double first backet"))
+print(mylist)
